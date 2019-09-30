20b8a774cea570afdc0f820fa76d9176942c18ac
dmitriy.trifonov <trifonov.d@pharm-portal.ru>
2019-09-30 16:32:34 +0300

    [SYN-45] list of un skus by company row index

--- indexes/company.py
+++ indexes/company.py
@@ -1,5 +1,13 @@
 from db.company import get_companies, get_companies_synonyms
+from db.company import get_addr_company_table
+# UN skus for indexing
+from db.un import get_en_skus
+from db.un import get_sku_uuid, get_sku_data
+
+from indexes.skus import build_sku_row_idx
+
 from helpers.strings import get_bow
+
 
 # @params items [(company_id, name),...]
 # => {company_id: row_num}
@@ -59,4 +67,87 @@ def build_company_idx():
   # rep(ddd)
   return dict(company_id_idx=company_id_idx,
               company_id_idx_inv=company_id_idx_inv,
-              terms_docs=terms_docs)
+              terms_docs=terms_docs,
+              companies_table=c_raw,
+              companies_synonyms=cs)
+
+# fail address id d8c7d99f-17e2-422e-a8dc-306fbb502483
+#  sku: [46127]51c219b3-071c-47b0-bd2f-3ede6f091649
+# fail address id d8c7d99f-17e2-422e-a8dc-306fbb502483
+#  sku: [50758]59a60901-340c-49f6-a47a-f01ce772cde5
+# fail address id d8c7d99f-17e2-422e-a8dc-306fbb502483
+#  sku: [132135]eb196502-7f24-4580-8337-3efc531cd194
+
+# aid = 'd8c7d99f-17e2-422e-a8dc-306fbb502483'
+# cid = '5616c61a-d00c-4d09-a9ab-d847226bb3b3'
+
+# add_to_company_idx(address_id=aid, company_id=cid, idx=addr_company_idx['addr_cmp'])
+def add_to_company_idx(address_id, company_id, idx):
+  idx[address_id] = company_id
+
+
+# from un sku
+def get_address_id(data: dict):
+  return data['address_id']
+
+
+### go through UN skus table
+### create
+### {..., [${company_row}]: [sku_doc_0, ..., sku_doc_n], ...}
+def build_skus_by_company_row():
+
+  #
+  # sku idx
+  #
+
+  un_skus = get_en_skus()
+
+  sku_indexes = build_sku_row_idx()
+  skus_id_rows_idx = sku_indexes['rows_id_inv']  # {..., [`${un_id}`]: row_num, ...}
+
+  #
+  # company
+  #
+
+  company_idx = build_company_idx()
+  company_id_idx = company_idx['company_id_idx']
+
+  addr_company_idx = get_addr_company_table()
+
+  # build_skus_by_company_row output:
+  # fail address id d8c7d99f-17e2-422e-a8dc-306fbb502483
+  #  sku: [46127]51c219b3-071c-47b0-bd2f-3ede6f091649
+  # fail address id d8c7d99f-17e2-422e-a8dc-306fbb502483
+  #  sku: [50758]59a60901-340c-49f6-a47a-f01ce772cde5
+  # fail address id d8c7d99f-17e2-422e-a8dc-306fbb502483
+  #  sku: [132135]eb196502-7f24-4580-8337-3efc531cd194
+
+  aid = 'd8c7d99f-17e2-422e-a8dc-306fbb502483'
+  cid = '5616c61a-d00c-4d09-a9ab-d847226bb3b3'
+
+  add_to_company_idx(address_id=aid, company_id=cid, idx=addr_company_idx['addr_cmp'])
+  company_id_by_address = addr_company_idx['addr_cmp']
+
+  row_docs = dict()
+  for sku in un_skus:
+    un_id = get_sku_uuid(sku)  # 00004ee5-aea0-4fb4-9d0b-a401584c6be5
+    sku_data = get_sku_data(sku)  # {"packs": {}, ..., "trade_name": "...", "address_id": "..."}
+    # doc_row by un_sku_id
+    un_sku_idx = skus_id_rows_idx[un_id]  # => 0 ; {..., [`${un_id}`]: row_num, ...}
+    address_id = get_address_id(sku_data)  # get sku.address_id == for ==> company_id
+    if address_id not in company_id_by_address:
+      print('fail address id {}\n sku: [{}]{}'.format(address_id, un_sku_idx, un_id))
+      continue
+    company_id = company_id_by_address[address_id]  # address_id => company_id
+    company_row_by_id = company_id_idx[company_id]  # company_id => row_company_id
+
+    # build dictionary
+    # row_num: docs_rows
+    docs = company_row_by_id in row_docs and row_docs[company_row_by_id] or list()
+
+    if un_sku_idx not in docs:
+      docs.append(un_sku_idx)
+      docs = sorted(docs)
+
+    row_docs[company_row_by_id] = docs
+  return row_docs
