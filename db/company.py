57089cb8ea55adf4777f7125d45b358d2a9f4a16
dmitriy.trifonov <trifonov.d@pharm-portal.ru>
2019-09-30 16:30:03 +0300

    [SYN-45] get company_id by addess_id from un skus;

--- db/company.py
+++ db/company.py
@@ -8,7 +8,7 @@ def get_companies():
     # 'select r."rowNumber", n."rskuId", n."iskuId" from "NSKUs" as n inner join "RSKUs" as r on n."rskuId"=r.id where n."type" = %s and n."status" = %s',
     # ('medicine', 'verified',))
   recs = cursor.fetchall()
-
+  conn.close()
   # verf_row_num_un_uuid = dict([(row_num, un_uuid) for row_num, rsku_id, un_uuid in recs])
   # verf_row_num_un_uuid
   return recs
@@ -18,5 +18,26 @@ def get_companies_synonyms():
   query = "select cs.company_id, cs.name from company_synonyms as cs order by cs.company_id;"
   cursor.execute(query)
   recs = cursor.fetchall()
+  conn.close()
   return recs
+
+def get_addr_company_table():
+  conn, cursor = pg_con(dbname='cmpny')
+  q = 'select a.id, a.company_id from addresses as a order by a.id;'
+  cursor.execute(q)
+  recs = cursor.fetchall()
+  conn.close()
+  conn = None
+  cursor = None
+
+  cmpy = dict()
+  addr = [(addr_id, c_id) for addr_id, c_id in recs]
+  for addr_id, c_id in recs:
+    docs = c_id in cmpy and cmpy[c_id] or list()
+    docs.append(addr_id)
+    cmpy[c_id] = docs
+
+  return dict(recs=recs,
+              addr_cmp=dict(addr),
+              cmp_addreses=cmpy)
 
